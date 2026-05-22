9314b7e09dce0c2b16c34892265fab85778fa405
pranav <pranavbhagwat17@gmail.com>
2026-05-22 09:29:05 +0530

    Adding Documentation

--- app/src/main/java/com/vaari/app/api/RetrofitClient.kt
+++ app/src/main/java/com/vaari/app/api/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.29.71:5000/"
+    private const val BASE_URL = "http://172.20.10.3:5000/"
 
     private val client = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
